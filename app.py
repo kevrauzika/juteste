f2c64f1b599a0e80bf8e8f20554805440f5d1903
kevrauzika <166714341+kevrauzika@users.noreply.github.com>
2024-07-22 13:44:31 -0300

    Atualizações para Vercel

--- app.py
+++ app.py
@@ -22,12 +22,15 @@ def index():
 @app.route('/add_product', methods=['GET', 'POST'])
 def add_product():
     if request.method == 'POST':
-        name = request.form['name']
-        price = float(request.form['price'])
-        products = load_products()
-        products.append({"name": name, "price": price})
-        save_products(products)
-        return redirect(url_for('index'))
+        try:
+            name = request.form['name']
+            price = float(request.form['price'])
+            products = load_products()
+            products.append({"name": name, "price": price})
+            save_products(products)
+            return redirect(url_for('index'))
+        except ValueError as e:
+            return f"Erro: {str(e)}", 400
     return render_template('add_product.html')
 
 @app.route('/calculate_price', methods=['GET', 'POST'])
@@ -37,16 +40,22 @@ def calculate_price():
     selected_products = []
     if request.method == 'POST':
         for product in products:
-            quantity_str = request.form.get(f'quantity_{product["name"]}', '0')
-            if quantity_str:
+            try:
+                quantity_str = request.form.get(f'quantity_{product["name"]}', '0')
                 quantity = float(quantity_str)
                 if quantity > 0:
                     total_cost += product['price'] * quantity
                     selected_products.append({"name": product["name"], "quantity": quantity, "price": product['price']})
-        hours_str = request.form['hours']
-        if hours_str:
+            except ValueError as e:
+                return f"Erro: {str(e)}", 400
+
+        try:
+            hours_str = request.form['hours']
             hours = float(hours_str)
             total_cost += hours * 10
+        except ValueError as e:
+            return f"Erro: {str(e)}", 400
+
     return render_template('calculate_price.html', products=products, total_cost=total_cost, selected_products=selected_products)
 
 if __name__ == "__main__":
